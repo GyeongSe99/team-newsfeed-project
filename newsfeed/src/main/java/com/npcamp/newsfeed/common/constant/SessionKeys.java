d34864f03a7906bc892318757df604c336c62da7
taeung515 <xodndd515@gmail.com>
2025-05-28 17:08:49 +0900

    chore: SessionKeys 클래스 추가 - 세션 키를 SESSION_USER_ID로 사용 resolves: #3

---
+++ newsfeed/src/main/java/com/npcamp/newsfeed/common/constant/SessionKeys.java
@@ -0,0 +1,11 @@
+package com.npcamp.newsfeed.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SessionKeys {
+
+    public static final String SESSION_USER_ID = "loginUserId";
+
+}
