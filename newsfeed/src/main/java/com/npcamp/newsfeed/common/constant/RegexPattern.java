9102777f265c135e4fc57f146e6a4e695065857f
taeung515 <xodndd515@gmail.com>
2025-05-28 23:43:00 +0900

    refactor: 비밀번호 정규 표현식 수정
    - 이전 정규표현식이 이메일형식으로 되어있어 수정
    
    resolves: #3

--- newsfeed/src/main/java/com/npcamp/newsfeed/common/constant/RegexPattern.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/common/constant/RegexPattern.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 public class RegexPattern {
 
     // 비밀번호는 8~16자리이며, 영문, 숫자, 특수문자를 최소 1개 이상 포함해야 합니다.
-    public static final String PASSWORD = "[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    public static final String PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$";
 
 }
