ea5fcba66336ccdd5beb63e66d29036d2250c319
천세경 <jay45652@gmail.com>
2025-05-28 15:40:09 +0900

    feat: UserEntity 추가
    resolves: #1

---
+++ newsfeed/src/main/java/com/npcamp/newsfeed/common/entity/User.java
@@ -0,0 +1,32 @@
+package com.npcamp.newsfeed.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+}
