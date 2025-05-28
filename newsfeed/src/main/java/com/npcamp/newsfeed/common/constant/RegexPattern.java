92cfc3d09e4fd5184bee49c05e0afbee2566eb20
taeung515 <xodndd515@gmail.com>
2025-05-28 18:14:50 +0900

    refactor: class부 final 제거
    resolves: #3

--- newsfeed/src/main/java/com/npcamp/newsfeed/common/constant/RegexPattern.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/common/constant/RegexPattern.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class RegexPattern {
+public class RegexPattern {
 
     // 비밀번호는 8~16자리이며, 영문, 숫자, 특수문자를 최소 1개 이상 포함해야 합니다.
     public static final String PASSWORD = "[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
--- newsfeed/src/main/java/com/npcamp/newsfeed/common/constant/SessionKeys.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/common/constant/SessionKeys.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class SessionKeys {
+public class SessionKeys {
 
     public static final String SESSION_USER_ID = "loginUserId";
 
