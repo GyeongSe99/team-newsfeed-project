c70b9f39cc935d9f1ad6b59c2069fc7fe9ba6325
dain <0901ekdls@gmail.com>
2025-05-29 17:23:44 +0900

    git commit -m "feat: 팔로우 생성 기능 구현
    resolves: #17"

--- newsfeed/src/main/java/com/npcamp/newsfeed/follow/controller/FollowController.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/follow/controller/FollowController.java
@@ -1,8 +1,29 @@
 package com.npcamp.newsfeed.follow.controller;
 
+import com.npcamp.newsfeed.follow.dto.FollowDto;
+import com.npcamp.newsfeed.follow.service.FollowService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/followers")
 public class FollowController {
-    // API 메서드 작성 예정
+
+    private final FollowService followService;
+
+    public FollowController(FollowService followService) {
+        this.followService = followService;
+    }
+
+    // 팔로우 생성 API 엔드포인트
+    @PostMapping
+    public ResponseEntity<FollowDto> createFollow(@RequestBody FollowDto followDto) {
+        FollowDto createdFollow = followService.createFollow(followDto);
+        return new ResponseEntity<>(createdFollow, HttpStatus.CREATED);
+    }
+
 }
--- newsfeed/src/main/java/com/npcamp/newsfeed/follow/repository/FollowRepository.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/follow/repository/FollowRepository.java
@@ -2,7 +2,9 @@ package com.npcamp.newsfeed.follow.repository;
 
 import com.npcamp.newsfeed.common.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     // 추가 쿼리 메서드 필요 시 여기에 작성
 }
