bbc6533ee718e1a038b60ae210326193839e7435
천세경 <jay45652@gmail.com>
2025-05-31 22:28:59 +0900

    feat: @Column 어노테이션을 추가하여 DB 검증 로직 추가
    resolves: #35

--- newsfeed/src/main/java/com/npcamp/newsfeed/common/entity/Comment.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/common/entity/Comment.java
@@ -18,6 +18,7 @@ public class Comment extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String content;
 
     private Long postId;
