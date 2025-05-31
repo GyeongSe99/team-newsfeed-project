978e39944d78a3a06785b0336b5a563be7c82d94
taeung515 <xodndd515@gmail.com>
2025-05-31 13:06:35 +0900

    refactor: LoginRequestDto email 필드에 final 추가
    resolves: #32

--- newsfeed/src/main/java/com/npcamp/newsfeed/auth/dto/LoginRequestDto.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/auth/dto/LoginRequestDto.java
@@ -17,6 +17,6 @@ public class LoginRequestDto {
 
     @Email(message = "이메일 형식을 확인해 주세요!")
     @NotBlank(message = "이메일을 입력해 주세요!")
-    private String email;
+    private final String email;
 
 }
