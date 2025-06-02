0336c91cd9d146745a41f20b7b396b05c24936df
taeung515 <xodndd515@gmail.com>
2025-06-02 15:45:13 +0900

    feat: 유효성 검증 예외 전역예외처리 추가
    - 응답형태 ApiResponse 통일
    
    resolves: #51

--- newsfeed/src/main/java/com/npcamp/newsfeed/common/exception/GlobalExceptionHandler.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/common/exception/GlobalExceptionHandler.java
@@ -4,6 +4,9 @@ import com.npcamp.newsfeed.common.payload.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,22 +14,40 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // NotFound Exception
     @ExceptionHandler(ResourceNotFoundException.class)
     protected ResponseEntity<ApiResponse<Void>> handleResourceNotFoundException(ResourceNotFoundException e) {
         HttpStatus status = e.getErrorCode().getStatus();
         return ResponseEntity.status(status).body(ApiResponse.failure(e.getErrorCode().getMsg()));
     }
 
+    // Conflict Exception
     @ExceptionHandler(ResourceConflictException.class)
     protected ResponseEntity<ApiResponse<Void>> handleResourceConflictException(ResourceConflictException e) {
         HttpStatus status = e.getErrorCode().getStatus();
         return ResponseEntity.status(status).body(ApiResponse.failure(e.getErrorCode().getMsg()));
     }
 
+    // Forbidden Exception
     @ExceptionHandler(ResourceForbiddenException.class)
     protected ResponseEntity<ApiResponse<Void>> handleResourceForbiddenException(ResourceForbiddenException e) {
         HttpStatus status = e.getErrorCode().getStatus();
         return ResponseEntity.status(status).body(ApiResponse.failure(e.getErrorCode().getMsg()));
     }
 
+    // MethodArgumentNotValidException Exception
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ApiResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append(fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(ApiResponse.failure(builder.toString()));
+    }
 }
