fbdf503ce8eb6e9b8961315313eebdbad8b0a7aa
천세경 <jay45652@gmail.com>
2025-06-02 18:39:43 +0900

    refactor: UserController & UserService 리팩토링
    - 비밀번호 일치 여부를 확인하는 메소드를 PasswordValidator 클래스로 분리
    - 코드만 표시되어있던 USER_ID -> 클래스명도 보이도록 RequestAttributeKey.USER_ID 로 수정
    - getUser 메소드에 @Transactional(readOnly = true) 추가
    - userRepository.save 메소드 호출하여 저장 과정을 명시하도록 수정
    - updateUser시 이름만 바뀌고 이메일은 바뀌지 않은 경우에도 예외가 발생하는 부분 검증 로직 변경
    
    resolves: #57

--- newsfeed/src/main/java/com/npcamp/newsfeed/user/controller/UserController.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.npcamp.newsfeed.user.controller;
 
+import com.npcamp.newsfeed.common.constant.RequestAttributeKey;
 import com.npcamp.newsfeed.common.payload.ApiResponse;
 import com.npcamp.newsfeed.user.dto.UpdatePasswordRequestDto;
 import com.npcamp.newsfeed.user.dto.UpdateUserRequestDto;
@@ -11,8 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import static com.npcamp.newsfeed.common.constant.RequestAttributeKey.USER_ID;
 
 /**
  * 회원 관련 기능을 담당하는 컨트롤러 클래스.
@@ -33,27 +32,22 @@ public class UserController {
     }
 
     @PatchMapping("/me")
-    public ResponseEntity<ApiResponse<UpdateUserResponseDto>> updateUser(
-            @RequestAttribute(USER_ID) Long id,
-            @Valid @RequestBody UpdateUserRequestDto requestDto
-    ) {
+    public ResponseEntity<ApiResponse<UpdateUserResponseDto>> updateUser(@RequestAttribute(RequestAttributeKey.USER_ID) Long loginUserId,
+                                                                         @Valid @RequestBody UpdateUserRequestDto requestDto) {
 
-        UpdateUserResponseDto responseDto =
-                userService.updateUser(
-                        id,
-                        requestDto.getName(),
-                        requestDto.getEmail(),
-                        requestDto.getPassword()
-                );
+        UpdateUserResponseDto responseDto = userService.updateUser(
+                loginUserId,
+                requestDto.getName(),
+                requestDto.getEmail(),
+                requestDto.getPassword()
+        );
 
         return new ResponseEntity<>(ApiResponse.success(responseDto), HttpStatus.OK);
     }
 
     @PatchMapping("/me/password")
-    public ResponseEntity<ApiResponse<Void>> updatePassword(
-            @RequestAttribute(USER_ID) Long id,
-            @Valid @RequestBody UpdatePasswordRequestDto requestDto
-    ) {
+    public ResponseEntity<ApiResponse<Void>> updatePassword(@RequestAttribute(RequestAttributeKey.USER_ID) Long id,
+                                                            @Valid @RequestBody UpdatePasswordRequestDto requestDto) {
         userService.updatePassword(id, requestDto.getOldPassword(), requestDto.getNewPassword());
         return new ResponseEntity<>(ApiResponse.success("비밀번호 변경이 완료되었습니다."), HttpStatus.OK);
     }
