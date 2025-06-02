17ee12e30136b7613ac03323a21c1742b191e940
dain <0901ekdls@gmail.com>
2025-06-03 01:33:11 +0900

    fix: 팔로우 생성 오류 수정
    - FollowRequestDto의 followerUserId를 삭제하여 제대로 입력해도 빈 것으로 인식되는 오류 수정
    
    resolves: #39

--- newsfeed/src/main/java/com/npcamp/newsfeed/follow/dto/FollowRequestDto.java
+++ newsfeed/src/main/java/com/npcamp/newsfeed/follow/dto/FollowRequestDto.java
@@ -10,9 +10,6 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class FollowRequestDto {
 
-    @NotNull(message = "팔로워 ID는 필수입니다.")
-    private Long followerUserId; // 팔로워
-
     @NotNull(message = "팔로잉 ID는 필수입니다.")
     private Long followeeUserId; // 팔로잉
 }
