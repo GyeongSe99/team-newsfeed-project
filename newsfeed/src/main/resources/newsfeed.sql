b0e575a42d8e48b4596eb615f1baf328dadc48af
taeung515 <xodndd515@gmail.com>
2025-06-03 13:13:14 +0900

    refactor: ddl-auto:none, .sql파일 추가
    resolves: #64

---
+++ newsfeed/src/main/resources/newsfeed.sql
@@ -0,0 +1,58 @@
+-- User 테이블
+CREATE TABLE user
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    name       VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    deleted    BIT NULL,
+    created_at DATETIME(6) NULL,
+    updated_at DATETIME(6) NULL
+);
+
+-- Follow 테이블
+CREATE TABLE follow
+(
+    id               BIGINT AUTO_INCREMENT PRIMARY KEY,
+    follower_user_id BIGINT NULL,
+    followee_user_id BIGINT NULL,
+    created_at       DATETIME(6) NULL,
+    updated_at       DATETIME(6) NULL,
+    FOREIGN KEY (follower_user_id) REFERENCES user (id),
+    FOREIGN KEY (followee_user_id) REFERENCES user (id)
+);
+
+-- Post 테이블
+CREATE TABLE post
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title      VARCHAR(255) NOT NULL,
+    content    LONGTEXT     NOT NULL,
+    writer_id  BIGINT NULL,
+    created_at DATETIME(6) NULL,
+    updated_at DATETIME(6) NULL,
+    FOREIGN KEY (writer_id) REFERENCES user (id)
+);
+
+-- Comment 테이블
+CREATE TABLE comment
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    content    VARCHAR(255) NOT NULL,
+    user_id    BIGINT NULL,
+    post_id    BIGINT NULL,
+    created_at DATETIME(6) NULL,
+    updated_at DATETIME(6) NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (post_id) REFERENCES post (id)
+);
+
+-- Post Like 테이블
+CREATE TABLE post_like
+(
+    post_like_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id      BIGINT NULL,
+    post_id      BIGINT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (post_id) REFERENCES post (id)
+);
